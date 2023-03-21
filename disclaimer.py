471f85d9a89f10f0808c3b615ba7483445479b26
Brian J King <brianjking@users.noreply.github.com>
2023-03-21 13:06:44 -0700

    Create disclaimer.py

---
+++ disclaimer.py
@@ -0,0 +1,13 @@
+        with gr.Box(visible=is_spaces):
+            if(is_spaces):
+                    gr.HTML(f'''
+                    <div>
+                        <p>🚧 Use of this beta version of our software is subject to and goverened by our beta software policy. Click here to view</p>
+                    </div>
+                ''')
+                else:
+                    gr.HTML(f'''
+                    <div>
+                        <p>🚧 Use of this beta version of our software is subject to and goverened by our beta software policy. Click here to view</p>
+                    </div>
+                ''')
